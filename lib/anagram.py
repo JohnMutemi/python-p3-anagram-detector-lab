83b83ac6cbcefda02f45734b725d481677924840
JohnMutemi <jmtutorsalp@gmail.com>
2024-05-27 13:16:37 +0300

    commit anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+# lib/anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, word_list):
+        sorted_word = sorted(self.word)
+        matches = [word for word in word_list if sorted(word) == sorted_word]
+        return matches
+
